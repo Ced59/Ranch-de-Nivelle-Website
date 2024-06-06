e3f2b583d47c73dceb975db14d108de3d5a390ed
ccaud <c.caudron59@gmail.com>
2024-06-06 15:09:40 +0200

    Add design for Meals

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -1,12 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RanchDuBonheur.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace RanchDuBonheur.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly RanchDbContext _context;
+
+        public EventController(RanchDbContext context)
         {
-            return View();
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(bool showPast = false)
+        {
+            var today = DateOnly.FromDateTime(DateTime.Today);
+            var meals = await _context.Meals
+                .Include(m => m.MealDishes)
+                .ThenInclude(md => md.Dish)
+                .Include(m => m.MealArtists)
+                .ThenInclude(ma => ma.Artist)
+                .Where(m => showPast ? m.Date < today : m.Date >= today)
+                .OrderBy(m => m.Date)  
+                .ToListAsync();
+
+            return View(new Tuple<List<RanchDuBonheur.Models.Pocos.Meals.Meal>, bool>(meals, showPast));
         }
     }
 }
